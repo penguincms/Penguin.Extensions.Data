b103eeffe1e46dc7edbb53cd07bdcd5f343889bc
AnLoffredo <38223101+AnLoffredo@users.noreply.github.com>
2019-08-08 10:22:28 -0700

    Add files via upload

---
+++ IEnumerableExtensions.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Linq;
+using System.Reflection;
+
+namespace Penguin.Extensions.Data
+{
+#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+    public static class IEnumerableExtensions
+#pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
+    {
+        #region Methods
+
+        /// <summary>
+        /// Converts an IEnumerable of objects to a data table, with property names as headers and values as items
+        /// </summary>
+        /// <param name="objList">The IEnumerable of objects to use as a data source</param>
+        /// <returns>A data table containing the object values</returns>
+        public static DataTable ToDataTable(this System.Collections.Generic.IEnumerable<object> objList)
+        {
+            DataTable thisTable = new DataTable();
+
+            Type objectType = objList.GetType().GenericTypeArguments[0];
+
+            List<PropertyInfo> Properties = new List<PropertyInfo>();
+
+            Dictionary<PropertyInfo, int> PropertyOrder = new Dictionary<PropertyInfo, int>();
+
+            foreach (PropertyInfo thisProp in objectType.GetProperties().Reverse())
+            {
+
+                int index = 0;
+
+                while (index < Properties.Count && PropertyOrder[Properties.ElementAt(index)] > index)
+                {
+                    index++;
+                }
+                Properties.Insert(index, thisProp);
+                PropertyOrder.Add(thisProp, index);
+
+            }
+
+            foreach (PropertyInfo thisProperty in Properties)
+            {
+                string DisplayName = string.Empty;
+                DisplayNameAttribute displayNameAttribute = thisProperty.GetCustomAttribute<DisplayNameAttribute>();
+                if (displayNameAttribute != null)
+                {
+                    DisplayName = displayNameAttribute.DisplayName;
+                }
+                else
+                {
+                    DisplayName = thisProperty.Name;
+                }
+
+                thisTable.Columns.Add(DisplayName);
+            }
+
+            foreach (object thisObj in objList)
+            {
+                DataRow thisRow = thisTable.NewRow();
+                int i = 0;
+                foreach (PropertyInfo thisProperty in Properties)
+                {
+                    thisRow[i++] = thisProperty.GetValue(thisObj);
+                }
+
+                thisTable.Rows.Add(thisRow);
+            }
+            return thisTable;
+        }
+
+        #endregion Methods
+    }
+}
---
+++ XNodeExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace Penguin.Extensions.Data
+{
+#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+    public static class XNodeExtensions
+#pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
+    {
+        #region Methods
+
+        /// <summary>
+        /// Returns a list of all child nodes by name
+        /// </summary>
+        /// <param name="node">The node to search</param>
+        /// <param name="childName">The child name to search for</param>
+        /// <returns>All children matching the name</returns>
+        public static List<XNode> GetAllChildrenByName(this XNode node, string childName) => (node as XElement).GetAllChildrenByName(childName);
+
+        /// <summary>
+        /// Returns a list of all child nodes by name
+        /// </summary>
+        /// <param name="node">The node to search</param>
+        /// <param name="childName">The child name to search for</param>
+        /// <returns>All children matching the name</returns>
+
+        public static List<XNode> GetAllChildrenByName(this XElement node, string childName) => node.DescendantNodes().Where(n => string.Equals((n as XElement)?.Name?.LocalName, childName, StringComparison.CurrentCultureIgnoreCase)).ToList();
+
+        #endregion Methods
+    }
+}
