1c65fab7cdcc233fa00abddfd81e47f1bb0c41f8
Anthony Loffredo <loffredo.ap@gmail.com>
2019-08-08 13:08:24 -0700

    ToCSV
    Re-adding ToCsv for data tables, and removing absolute paths

---
+++ DataTableExtensions.cs
@@ -0,0 +1,39 @@
+﻿using System.Data;
+using System.Text;
+
+namespace Penguin.Extensions.Data
+{
+#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+    public static class DataTableExtensions
+#pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
+    {
+        /// <summary>
+        /// Writes a DataTable to a CSV file specified by the path
+        /// </summary>
+        /// <param name="dataTable">The datatable to write</param>
+        /// <param name="filePath">The location of the CSV to create</param>
+        public static void ToCSV(this DataTable dataTable, string filePath)
+        {
+            StringBuilder fileContent = new StringBuilder();
+
+            foreach (object col in dataTable.Columns)
+            {
+                fileContent.Append(col.ToString() + ",");
+            }
+
+            fileContent.Replace(",", System.Environment.NewLine, fileContent.Length - 1, 1);
+
+            foreach (DataRow dr in dataTable.Rows)
+            {
+                foreach (object column in dr.ItemArray)
+                {
+                    fileContent.Append("\"" + column.ToString() + "\",");
+                }
+
+                fileContent.Replace(",", System.Environment.NewLine, fileContent.Length - 1, 1);
+            }
+
+            System.IO.File.WriteAllText(filePath, fileContent.ToString());
+        }
+    }
+}
