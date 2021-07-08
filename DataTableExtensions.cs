f8128438e9c78aa883f1f1edffe6a48b7db9e747
AnLoffredo <38223101+AnLoffredo@users.noreply.github.com>
2021-07-08 14:50:23 -0700

    Autocommit
    Autocommit

--- DataTableExtensions.cs
+++ DataTableExtensions.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using Penguin.FileStreams;
+using Penguin.FileStreams.Interfaces;
+using System;
 using System.Data;
 using System.Diagnostics.Contracts;
+using System.IO;
+using System.IO.Compression;
 using System.Linq;
 using System.Reflection;
 using System.Text;
@@ -17,63 +21,65 @@ namespace Penguin.Extensions.Data
         /// </summary>
         /// <param name="dataTable">The datatable to write</param>
         /// <param name="filePath">The location of the CSV to create</param>
-        /// <param name="includeHeaders">Write column headers to the output string</param>
-        /// <param name="quoteValues">Should the values be quoted</param>
-        public static void ToCsv(this DataTable dataTable, string filePath, bool includeHeaders = true, bool quoteValues = true) => System.IO.File.WriteAllText(filePath, dataTable.ToCsv(includeHeaders, quoteValues));
-
-        /// <summary>
-        /// Writes a DataTable to a CSV file specified by the path
-        /// </summary>
-        /// <param name="dataTable">The datatable to write</param>
-        /// <param name="filePath">The location of the CSV to create</param>
-        /// <param name="includeHeaders">Write column headers to the output string</param>
-        /// <param name="quoteValues">Should the values be quoted</param>
-        [Obsolete("Use ToCsv", false)]
-        public static void ToCSV(this DataTable dataTable, string filePath, bool includeHeaders = true, bool quoteValues = true) => System.IO.File.WriteAllText(filePath, dataTable.ToCsv(includeHeaders, quoteValues));
-
-        /// <summary>
-        /// Writes a datatable as a CSV string and returns the string
-        /// </summary>
-        /// <param name="dataTable">The data source</param>
-        /// <returns>A CSV representation of the data table</returns>
-        /// <param name="includeHeaders">Write column headers to the output string</param>
-        /// <param name="quoteValues">Should the values be quoted</param>
-        [Obsolete("Use ToCsv", false)]
-        public static string ToCSV(this DataTable dataTable, bool includeHeaders = true, bool quoteValues = true) => dataTable.ToCsv(includeHeaders, quoteValues);
-
-        public static string ToCsvRow(this DataRow dr, bool quoted = true)
+        /// <param name="settings">Optional csv settings</param>
+        public static void ToCsv(this DataTable dataTable, string filePath, ToCsvSettings settings = null)
         {
+            settings = settings ?? new ToCsvSettings();
+
+            using (IFileWriter fileWriter = FileWriterFactory.GetFileWriter(filePath, settings.Compression))
+            {
+                fileWriter.Write(dataTable.ToCsv(settings));
+            }
+        }
+
+        /// <summary>
+        /// Creates a CSV row from the provided data row
+        /// </summary>
+        /// <param name="dr">The data row to convert</param>
+        /// <param name="quoteCharacter">An optional character to use for quoting items</param>
+        /// <returns></returns>
+        public static string ToCsvRow(this DataRow dr, char? quoteCharacter = '"')
+        {
+
+
             if (dr is null)
             {
                 throw new ArgumentNullException(nameof(dr));
             }
 
+            string qReplace = null;
+
+            if (quoteCharacter.HasValue)
+            {
+                qReplace = $"{quoteCharacter.Value}{quoteCharacter.Value}";
+            }
+
             StringBuilder sb = new StringBuilder();
 
-            for(int i = 0; i < dr.ItemArray.Length; i++)
+            for (int i = 0; i < dr.ItemArray.Length; i++)
             {
-                if(i != 0)
+                if (i != 0)
                 {
                     sb.Append(',');
                 }
 
-                if(quoted)
+                if (quoteCharacter.HasValue)
                 {
-                    sb.Append('"');
+                    sb.Append(quoteCharacter.Value);
                 }
 
                 string oVal = ObjectToString(dr.ItemArray[i]);
 
-                if(quoted)
+                if (quoteCharacter.HasValue)
                 {
-                    oVal = oVal.Replace("\"", "\"\"");
+                    oVal = oVal.Replace($"{quoteCharacter.Value}", qReplace);
                 }
 
                 sb.Append(oVal);
 
-                if(quoted)
+                if (quoteCharacter.HasValue)
                 {
-                    sb.Append('"');
+                    sb.Append(quoteCharacter.Value);
                 }
             }
 
@@ -85,16 +91,18 @@ namespace Penguin.Extensions.Data
         /// </summary>
         /// <param name="dataTable">The data source</param>
         /// <returns>A CSV representation of the data table</returns>
-        /// <param name="includeHeaders">Write column headers to the output string</param>
-        /// <param name="quoteValues">Should the values be quoted</param>
-        public static string ToCsv(this DataTable dataTable, bool includeHeaders = true, bool quoteValues = true)
+        /// <param name="settings">Optional csv settings</param>
+        public static string ToCsv(this DataTable dataTable, ToCsvSettings settings = null)
         {
             Contract.Requires(dataTable != null);
 
             StringBuilder fileContent = new StringBuilder();
 
             bool firstLine = true;
-            if (includeHeaders)
+
+            settings = settings ?? new ToCsvSettings();
+
+            if (settings.IncludeHeaders)
             {
                 fileContent.Append(string.Join(",", dataTable.Columns.Cast<DataColumn>())); //ToString for the header name?
                 firstLine = false;
@@ -106,8 +114,8 @@ namespace Penguin.Extensions.Data
                 {
                     fileContent.Append(System.Environment.NewLine);
                 }
-                
-                fileContent.Append(dr.ToCsvRow(quoteValues));
+
+                fileContent.Append(dr.ToCsvRow(settings.QuoteCharacter));
 
                 firstLine = false;
             }
@@ -127,6 +135,13 @@ namespace Penguin.Extensions.Data
             }
         }
 
+        /// <summary>
+        /// Adds an item to the data row, adding the column if needed. 
+        /// If the column exists, updates the existing value
+        /// </summary>
+        /// <param name="dr">The data row to update</param>
+        /// <param name="column">The column to target</param>
+        /// <param name="value">The value to add or update</param>
         public static void AddOrUpdate(this DataRow dr, string column, object value)
         {
             if (dr is null)
@@ -138,6 +153,12 @@ namespace Penguin.Extensions.Data
             dr[column] = value;
         }
 
+        /// <summary>
+        /// Adds an object to the data table as a data row, and then returns the new row
+        /// </summary>
+        /// <param name="dt">The data table to target</param>
+        /// <param name="toAdd">The object to add to the table</param>
+        /// <returns>The newly created data row containing the object information</returns>
         public static DataRow Add(this DataTable dt, object toAdd)
         {
             if (dt is null)
@@ -174,6 +195,13 @@ namespace Penguin.Extensions.Data
             return newRow;
         }
 
+        /// <summary>
+        /// Returns true if the data table contains the requested column
+        /// </summary>
+        /// <param name="dt">The data table to check</param>
+        /// <param name="columnName">The column to check for</param>
+        /// <param name="comparison">An optional string comparison to use when checking for the column name. Defaults to OrdinalIgnoreCase</param>
+        /// <returns>True if a column with a matching name is found</returns>
         public static bool ContainsColumn(this DataTable dt, string columnName, StringComparison comparison = StringComparison.OrdinalIgnoreCase)
         {
             if (dt is null)
@@ -189,6 +217,11 @@ namespace Penguin.Extensions.Data
             return dt.Columns.Cast<DataColumn>().Any(dc => string.Equals(columnName, dc.ColumnName, comparison));
         }
 
+        /// <summary>
+        /// Sets up a data table to have columns representative of properties found on a given type
+        /// </summary>
+        /// <param name="dt">The data table to scaffold</param>
+        /// <param name="toScaffold">The type to use to create the columns on the table</param>
         public static void Scaffold(this DataTable dt, Type toScaffold)
         {
             if (dt is null)
@@ -209,6 +242,14 @@ namespace Penguin.Extensions.Data
                 }
             }
         }
+
+        /// <summary>
+        /// Creates a column on a table if it doesn't already exist
+        /// </summary>
+        /// <param name="dt">The data table to target</param>
+        /// <param name="columnName">The name of the column to ensure</param>
+        /// <param name="columnType">The type of the data contained in the column</param>
+        /// <returns>True if the column already existed. False if it was created</returns>
         public static bool EnsureColumn(this DataTable dt, string columnName, Type columnType = null)
         {
             if (dt is null)
---
+++ ToCsvSettings.cs
@@ -0,0 +1,26 @@
+﻿using Penguin.FileStreams;
+
+namespace Penguin.Extensions.Data
+{
+    /// <summary>
+    /// Settings to use when converting a DataTable to a CSV
+    /// </summary>
+    public class ToCsvSettings
+    {
+        /// <summary>
+        /// Serialize the headers to the first row of the file. Default true
+        /// </summary>
+        public bool IncludeHeaders { get; set; } = true;
+
+        /// <summary>
+        /// An optional character to use to quote items. defaults to "
+        /// </summary>
+        public char? QuoteCharacter { get; set; } = '"';
+
+        /// <summary>
+        /// Compression to use when writing CSV files. Default None.
+        /// The output file will have the correct extension appended
+        /// </summary>
+        public FileStreamCompression Compression { get; set; } = FileStreamCompression.None;
+    }
+}
